b5cec85c70094b8766e710b84d2ee4a3ead8e54c
bweick <brian.weickmann@gmail.com>
2024-07-10 11:29:45 -0700

    Update erring tests.

--- test/project-registrars/PBTSimpleProjectRegistrar.spec.ts
+++ test/project-registrars/PBTSimpleProjectRegistrar.spec.ts
@@ -42,7 +42,7 @@ import { Blockchain } from "@utils/common";
 import { BigNumber } from "ethers";
 const expect = getWaffleExpect();
 
-describe("PBTSimpleProjectRegistrar", () => {
+describe.only("PBTSimpleProjectRegistrar", () => {
   let owner: Account;
   let developerOne: Account;
   let developerTwo: Account;
@@ -65,6 +65,7 @@ describe("PBTSimpleProjectRegistrar", () => {
   let chipRegistry: ChipRegistryMock;
   let transferPolicy: OpenTransferPolicy;
   let newTransferPolicy: Account;
+  let developerRegistrarImpl: DeveloperRegistrar;
   let developerRegistrar: DeveloperRegistrar;
 
   let manufacturerId: string;
@@ -152,11 +153,15 @@ describe("PBTSimpleProjectRegistrar", () => {
     servicesRegistry = await deployer.deployServicesRegistry(chipRegistry.address);
 
     // 8. Deploy Developer Registrar Factory
-    developerRegistrarFactory = await deployer.deployDeveloperRegistrarFactory(
+    developerRegistrarImpl = await deployer.deployDeveloperRegistrar(
       chipRegistry.address,
       ersRegistry.address,
       developerRegistry.address,
       servicesRegistry.address
+    );
+    developerRegistrarFactory = await deployer.deployDeveloperRegistrarFactory(
+      developerRegistrarImpl.address,
+      developerRegistry.address
     );
 
     // 9. Initialize Chip Registry
